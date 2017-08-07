5d68a35e2871ed2faee35a32c6b0f7c7a1e20584
Steven Meads <sbmeads@gmail.com>
2017-08-07 14:55:18 -0700

    (add) more unit tests for challenges

--- test/tests.js
+++ test/tests.js
@@ -3,19 +3,46 @@ require('babel-register')({
 });
 
 const expect = require('expect');
-const challenges = require ('../challenges');
 
 // test for sum-all-range
 describe('sumAll', () => {
   it ('should return the sum of the range of input array', () => {
-    const sumAll = challenges.sumAll;
+    const sumAll = require ('../challenges/sum-all-range');
 
-    expect(sumAll([1, 4]).toEqual(Number));
-    expect(sumAll([1, 4]).toEqual(10));
-    expect(sumAll([4, 1]).toEqual(10));
-    expect(sumAll([5, 10]).toEqual(10));
-    expect(sumAll([10, 5]).toEqual(10));
+    expect(sumAll([1, 4]).to.equal(Number));
+    expect(sumAll([1, 4]).to.equal(10));
+    expect(sumAll([4, 1]).to.equal(10));
+    expect(sumAll([5, 10]).to.equal(45));
+    expect(sumAll([10, 5]).to.equal(45));
   })
 });
 
-// test for linked-list-cycles
+// Test for linked-list-cycles
+describe('linkedListCycles', () => {
+  it ('should return true if the linked list has a cyclical reference', () => {
+    const linkedListCycles = require ('../challenges/linked-list-cycles');
+  })
+});
+
+// Test for first-duplicate
+describe('firstDuplicate', () => {
+  it ('should return the first duplicate number.', () => {
+    const firstDuplicate = require ('../challenges/first-duplicate');
+  })
+});
+
+// Test for reverse-in-place
+describe('reverseInPlace', () => {
+  it ('should reverse an array in place', () => {
+    const reverseInPlace = require ('../challenges/reverse-in-place');
+  })
+});
+
+// Test for unique-number
+describe('uniqueNumber', () => {
+  it ('should print input array to the console', () => {
+    const uniqueNumber = require ('../challenges/unique-number');
+
+    expect(uniqueNumber([1, 4]).toEqual(console.log([1, 4])));
+  })
+});
